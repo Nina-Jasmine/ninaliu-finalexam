c532590c5a9e1811348e2c3548739373e32e1c2d
Nina-Jasmine <82427284+Nina-Jasmine@users.noreply.github.com>
2025-12-11 20:28:49 -0500

    added yaml file and configured swagger UI

--- app.js
+++ app.js
@@ -1,43 +1,69 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const mongoose = require('mongoose');
 const globals = require('./globals');
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Routers
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const apiRestaurantsRouter = require('./routes/api/restaurants');
 
-var app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
+const app = express();
 
+ 
 // Connect to MongoDB
+// ------------------------
 mongoose.connect(globals.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
     console.error('Failed to connect to MongoDB', err);
-    process.exit(1);
+    process.exit(1); // Exit if DB connection fails
   });
 
-  // Swagger setup
+// ------------------------
+// Swagger UI setup
+// ------------------------
+try {
+  const swaggerDocument = YAML.load('./swagger.yaml');
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (err) {
+  console.error('Failed to load Swagger document:', err);
+}
 
-  const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-
-const swaggerDocument = YAML.load('./swagger.yaml');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));    
-
- 
+// ------------------------
+// Middleware
+// ------------------------
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ------------------------
+// Routes
+// ------------------------
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/restaurants', apiRestaurantsRouter);
+
+// ------------------------
+// 404 handler
+// ------------------------
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Resource not found' });
+});
+
+// ------------------------
+// Error handler
+// ------------------------
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({ error: err.message });
+});
 
 module.exports = app;
